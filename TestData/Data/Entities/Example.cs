4ff0a37e0c38af12598f6d500288e897a2e05fe6
dany1468 <dany1468@gmail.com>
2021-04-29 08:32:46 +0900

    task ef:update

---
+++ TestData/Data/Entities/Example.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace TestData.Data.Entities
+{
+    public partial class Example
+    {
+        public int Id { get; set; }
+        public int ExampleInt { get; set; }
+        public bool ExampleBit { get; set; }
+    }
+}
---
+++ TestData/Data/Entities/MainDbContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace TestData.Data.Entities
+{
+    public partial class MainDbContext : DbContext
+    {
+        public MainDbContext()
+        {
+        }
+
+        public MainDbContext(DbContextOptions<MainDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Example> Example { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=host.docker.internal,14331;Database=main;User Id=sa;Password=P@55w0rd;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
